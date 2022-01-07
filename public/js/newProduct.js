9ad8f1a2190f601252f9d808db784209436a5a59
MATPL <matthew.proctor-leake@nhs.net>
2022-01-07 07:38:21 +0000

    move back to summary js added for date/name/has spc

--- public/js/newProduct.js
+++ public/js/newProduct.js
@@ -1,3 +1,5 @@
+console.log(localStorage);
+
 var getContinueButton = document.getElementsByClassName("nhsuk-button")[0];
 var getPackShdDisPrice= localStorage.getItem("pack-should-display-price");
 
@@ -46,8 +48,8 @@ function storeName(){
 var getProductName = localStorage.getItem("product-name");
 var prodName = document.querySelector("#product-name");
 
-
 if (prodName != null) {
+console.log(prodName);
   if (getProductName != null) {
     prodName.defaultValue = getProductName;
   }
@@ -55,3 +57,75 @@ if (prodName != null) {
     getContinueButton.href = "product-summary";
   }
 }
+
+var radioButtons = document.getElementsByName("data-spc");
+
+if (radioButtons) {
+var getProductSpc = localStorage.getItem("product-spc");
+var buttonLink = document.getElementById("continue-button");
+  if (getProductSpc != null) {
+    for (var radio of radioButtons) {
+      if (getProductSpc === radio.value) {
+     radio.checked = true;
+            }
+      if (radio.value == 'yes' && getPackShdDisPrice != null) {
+          buttonLink.href = "product-summary";
+            }
+          }
+        }
+      };
+
+function storeSpcYesOrNo() {
+  var radioButtons = document.getElementsByName("data-spc");
+  var buttonLink = document.getElementById("continue-button");
+  console.log(buttonLink);
+
+  for (var radio of radioButtons) {
+      if (radio.checked) {
+          localStorage.setItem("product-spc", radio.value);
+         if (radio.value == 'yes' && getPackShdDisPrice == null) {
+          buttonLink.href = "product-spc-upload-choice";
+          } else if (radio.value == 'no') {
+          buttonLink.href = "product-no-spc";
+          }
+        }
+     }
+  }
+
+  var radioButtons = document.getElementsByName("link");
+
+  if (radioButtons != null) {
+  var getProductLink = localStorage.getItem("product-link-upload");
+  var inputLink = document.getElementById("conditional-contact-1");
+  var inputLinkUrl = document.getElementById("link-url");
+      if (getProductLink != null) {
+            for (var radio of radioButtons) {
+                  if (getProductLink == radio.value) {
+                          radio.checked = true;
+                  if (radio.value == 'link') {
+                         inputLink.classList.remove("nhsuk-radios__conditional--hidden");
+                         inputLinkUrl.defaultValue = localStorage.getItem("product-link-url")
+                      }
+                  }
+               }
+           }
+       };
+
+  function storeLinkOrUpload() {
+    var radioButtons = document.getElementsByName("link");
+    var buttonLink = document.getElementById("continue-button");
+    var inputLink = document.getElementById("link-url");
+
+    for (var radio of radioButtons) {
+        if (radio.checked) {
+            localStorage.setItem("product-link-upload", radio.value);
+           if (localStorage.getItem("product-link-upload") == 'upload') {
+            buttonLink.href = "product-file-upload";
+            }
+            if (localStorage.getItem("product-link-upload") == 'link') {
+            localStorage.setItem("product-link-url", inputLink.value);
+            buttonLink.href = "../product-pack/pack-size-strength";
+          }
+          }
+       }
+    }
