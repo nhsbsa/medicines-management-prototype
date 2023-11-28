c0c8ccb33abdb1d5c7f2437c9c457017ebdb02c6
MATPL <matthew.proctor-leake@nhs.net>
2023-11-28 17:06:19 +0000

    Creating profile section

--- gulpfile.js
+++ gulpfile.js
@@ -69,13 +69,13 @@ gulp.task('gov-toolkit-install-js', () => {
 });
 
 gulp.task('nhs-toolkit-install-js', () => {
-  return gulp.src('node_modules/nhsuk-frontend/dist/nhsuk-7.1.0.min.js')
+  return gulp.src('node_modules/nhsuk-frontend/dist/nhsuk-7.0.0.min.js')
   .pipe(rename("nhsuk.min.js"))
   .pipe(gulp.dest('./public/js'));
 });
 
 gulp.task('nhs-toolkit-install-css', () => {
-  return gulp.src('node_modules/nhsuk-frontend/dist/nhsuk-7.1.0.min.css')
+  return gulp.src('node_modules/nhsuk-frontend/dist/nhsuk-7.0.0.min.css')
   .pipe(rename("nhsuk.min.css"))
   .pipe(gulp.dest('./public/css'));
 });
